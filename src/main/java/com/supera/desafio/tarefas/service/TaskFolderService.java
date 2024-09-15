265d9666e547befa5a7f54ab25d01a7ce47bdb92
EriqueRocha <eriquebit@gmail.com>
2024-09-15 13:47:10 -0300

    ajuste importação

--- src/main/java/com/supera/desafio/tarefas/service/TaskFolderService.java
+++ src/main/java/com/supera/desafio/tarefas/service/TaskFolderService.java
@@ -9,14 +9,11 @@ import com.supera.desafio.tarefas.repository.TaskRepository;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class TaskFolderService {
